39676a2b83f75825ada5a5c9be6b130d2cec2b9b
rifandani <ipandani2505@gmail.com>
2020-01-22 23:40:41 +0700

    uncomment

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
  </form>
  <?php
     
-/*    // PHP Data Objects(PDO) Sample Code:
+    // PHP Data Objects(PDO) Sample Code:
     try {
         $conn = new PDO("sqlsrv:server = tcp:rifandaniwebappserver.database.windows.net,1433; Database = webappdb", "rifandani", "098765Aa");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -36,7 +36,7 @@
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
-*/    
+    
     
     // SQL Server Extension Sample Code:
     $connectionInfo = array("UID" => "rifandani", "pwd" => "098765Aa", "Database" => "webappdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
